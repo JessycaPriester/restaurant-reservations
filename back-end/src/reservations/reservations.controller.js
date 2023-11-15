45f55f5b64fb6c28e3c4aadfe7a170ac08020c46
JessycaPriester <jessyca_priester@yahoo.com>
2023-11-15 15:57:40 -0500

    code review

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,32 +1,18 @@
 const knex = require("../db/connection")
 const reservations = require('../db/seeds/00-reservations.json')
+const service = require("./reservations.service")
 /**
  * List handler for reservation resources
  */
-async function list(req, res) {
-  const date = req.query.date;
-
-  if (!date) {
-    return res.status(400).json({ error: 'Missing date parameter' });
-  }
-
-  if (reservations) {
-    try {
-      // Assuming you have a 'reservations' table in your database
-      const data = await knex('reservations')
-        .where('reservation_date', '=', date)
-        .select();
-
-      if (data.length > 0) {
-        res.json({ data });
-      } else {
-        res.json({ message: 'No matching reservation found.' });
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  }
+async function list(request, response) {
+  const date = request.query.date;
+  console.log(date)
+  //const mobile_number = request.query.mobile_number;
+  const reservations = await service.list(date);
+  const res = reservations.filter(
+    (reservation) => reservation.status !== "finished"
+  );
+  response.json({ data: res });
 }
 
 module.exports = {
--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,7 +1,12 @@
 const knex = require("../db/connection")
 
-function list() {
-    return knex("reservations").select("*");
+function list(date) {
+    if (date) {
+    return knex("reservations")
+    .select("*")
+    .where({ reservation_date: date })
+    .orderBy("reservation_time", "asc");
+    }
 }
 
 module.exports = {
